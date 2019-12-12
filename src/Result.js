84f6fae8852326fd0f8b0d033171bf4de81b9ed2
mase4506 <47620212+mase4506@users.noreply.github.com>
2019-12-12 02:27:33 -0700

    css additions

--- src/Result.js
+++ src/Result.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-
+import './App.css';
 
 class Result extends Component {
 	render() {
-		var imageUrl = "http://image.tmdb.org/t/p/w342" + this.props.backdrop;
+		var imageUrl;
+		if(this.props.backdrop === "null")
+		{
+			imageUrl = "noImage.png";
+		}
+		else
+		{
+			imageUrl = "http://image.tmdb.org/t/p/w342" + this.props.backdrop;
+		}
+
 		var resultStyle={
 			display: "flex",
 			flexDirection: "column",
@@ -13,27 +22,30 @@ class Result extends Component {
 			border: "1px solid black",
 			borderRadius: "5px",
 			backgroundImage: "url('" + imageUrl + "')",
-			color: "white"
+			color: "white",
+			backgroundColor: "gray"
 		}
 		var titleStyle={
 			width: "100%",
 			alignSelf: "flex-end",
 			fontSize: "1.1em",
-			marginTop: "10px"
+			marginTop: "10px",
+			padding: "0 5px",
+			marginBottom: "10px"
 		}
 		var overviewStyle={
 			padding: "0 5px",
 			maxHeight: "75px",
 			alignSelf: "flex-end",
 			overflow: "hidden",
-			fontSize: ".8em"
+			fontSize: ".8em",
+			fontStyle: 'italic'
 		}
 		return(
-			<div>
-				<div style={resultStyle} className="result">
-					<div style={overviewStyle}>{this.props.overview}</div>
-					<div style={titleStyle}>{this.props.title}</div>
-				</div>
+			<div className="movieResults">
+				<div style={resultStyle} className="result"></div>
+				<div style={titleStyle}>{this.props.title}</div>
+				<div style={overviewStyle}>{this.props.overview}</div>
 			</div>
 		);
 	}
